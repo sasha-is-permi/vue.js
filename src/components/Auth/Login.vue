e473431648748d03b67745ae5d5f4f8ab142250f
Alexandr <sasha-is-permi@mail.ru>
2020-10-26 16:43:48 +0500

    '8'

--- src/components/Auth/Login.vue
+++ src/components/Auth/Login.vue
@@ -1,17 +1,82 @@
 <template>
-<!-- любой элемент с такого тега начинаться будет 
- -->
-  <v-container>
-   <!-- row- Выравниваем layout по строке
-   аттрибут vuetify создает свойство 
-   flex-direction:row 
-   xs12 - размер div, адаптация -->   
-    <v-layout row xs12>
-        <v-flex>
-            <h1>Login</h1>
-        </v-flex>
-    </v-layout>
-  </v-container>
+<!--elevation - добавляет box shadow на элемент.
+Flex центрирование формы:
+    align="center"
+    justify="center"
+  
+    12 колонок форма занимает- для маленьких экранов совсем
+    Начиная с sm-8, начиная с md- 6.
+     cols="12"
+     sm="8"
+     md="6"
+
+    prepend-icon="mdi-account" - какая иконка должна быть вставленна
+    перед текстовым input.
+
+    label="Login" - то, что написанно  в поле ввода. 
+
+
+-->
+
+        <v-container
+          class="fill-height"
+          fluid
+        >
+          <v-row
+            align="center"
+            justify="center"
+          >
+            <v-col
+              cols="12"
+              sm="8"
+              md="6"
+            >
+              <v-card class="elevation-12">
+                <v-toolbar
+                  color="primary"
+                  dark
+                  flat
+                >
+                  <v-toolbar-title>Login form</v-toolbar-title>
+                                  
+                </v-toolbar>
+                <v-card-text>
+                  <v-form v-model="valid" ref="form" validation>
+                    <v-text-field
+                      label="Email"
+                      name="email"
+                      prepend-icon="mdi-account"
+                      type="email"
+                      v-modal="email"
+                      :rules="emailRules"
+                    ></v-text-field>
+  
+                    <v-text-field
+                      id="password"
+                      label="Password"
+                      name="password"
+                      prepend-icon="mdi-lock"
+                      type="password"
+                      :counter="6"
+                      v-modal="password"
+                    ></v-text-field>
+                  </v-form>
+                </v-card-text>
+                <!-- Под формой- для отправки формы. 
+                v-spacer кнопочку Login перемещает вправо.
+                При клике на кнопочку "Login"- 
+                вызываем метод onSubmit  -->
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn
+                  @click="onSubmit"
+                  color="primary"
+                  >Login</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
 </template>
 
 <script>
@@ -19,9 +84,22 @@
 export default {
 
 data () {
-return {}
+return {
+   email:'',
+   password:'',
+   valid:false,
+       emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+/.test(v) || 'E-mail must be valid',
+      ]
 } 
-}
+},
+methods: {
+onSubmit() {
+// login
 
+}
+}
+}
 
 </script>
