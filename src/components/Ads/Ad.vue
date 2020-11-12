d14edbd7034b78feef1d6f520f7234da8e12e14b
Alexandr <sasha-is-permi@mail.ru>
2020-11-13 01:38:21 +0500

    '14'

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -6,9 +6,33 @@
    аттрибут vuetify создает свойство 
    flex-direction:row 
    xs12 - размер div, адаптация -->   
-    <v-layout row xs12>
-        <v-flex>
-            <h1>Ad</h1>
+    <v-layout row>
+        <v-flex xs12>
+       
+         <v-card>
+            
+            <v-img
+            src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"
+            height="300px"
+            >
+
+
+            </v-img>
+
+            <v-card-text>
+              <h1 class="text--primary"> Заголовок </h1>
+              <p> Текст заголовка </p>
+
+            </v-card-text>
+
+            <v-card-actions>
+                 <v-spacer>  </v-spacer>                
+                 <v-btn class="warning"> Edit  </v-btn>
+                 <v-btn class="success"> Buy   </v-btn>
+            </v-card-actions>
+
+         </v-card>
+
         </v-flex>
     </v-layout>
   </v-container>
--- src/components/Ads/AdList.vue
+++ src/components/Ads/AdList.vue
@@ -1,25 +1,86 @@
-<template>
+  <template>
 <!-- любой элемент с такого тега начинаться будет 
  -->
   <v-container>
    <!-- row- Выравниваем layout по строке
    аттрибут vuetify создает свойство 
    flex-direction:row 
-   xs12 - размер div, адаптация -->   
-    <v-layout row xs12>
-        <v-flex>
-            <h1>AdList</h1>
-        </v-flex>
-    </v-layout>
+   xs12 - размер div, адаптация 
+   sm6 offset-sm3 - центрирование получается.
+   
+   -->   
+  <v-flex xs12 sm6 offset-sm3>
+  <h1 class="text-secondary mb-3"> My ads </h1>
+
+<!-- elevation-10 - тень -->
+  <v-card
+  class="elevation-10 mb-5"
+  v-for="ad in ads"
+  :key="ad.id"
+  >
+  <v-layout row >
+    
+      <v-flex xs4>
+         <!-- Выводим список созданных объявлений-->
+          <v-img
+            :src="ad.imageSrc"
+            height="160px"
+          >
+ 
+          </v-img>
+        
+      </v-flex>
+
+      <v-flex xs8>
+       <!-- Выводим список созданных объявлений-->
+          <v-card-text>
+            <h2 class="text-primary"> {{ad.title}}</h2>     
+            <p> {{ad.description}} </p>
+          </v-card-text>
+
+          <v-card-actions>
+             <v-spacer></v-spacer>  
+             <v-btn
+              class="info mr-3"
+              :to="'ad/' + ad.id"
+              >  Open </v-btn> 
+          </v-card-actions> 
+      </v-flex>
+
+     
+  </v-layout>
+  </v-card>
+
+  </v-flex>
   </v-container>
-</template>
+  </template>
 
 <script>
 
 export default {
 
 data () {
-return {}
+return {
+  ads: [
+   {title:'Фото 1',
+     description:'Белочка',
+     promo: false,
+     imageSrc:'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+     id:'1'},
+   {title:'Фото 2',
+     description:'Фото неба',
+     promo: true,
+     imageSrc:'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+     id:'2'},
+   {title:'Фото 3',
+      description:'Зима',
+      promo: false,
+      imageSrc:'https://cdn.vuetifyjs.com/images/cards/docks.jpg',
+      id:'3'}
+       ]
+
+
+}
 } 
 }
 
